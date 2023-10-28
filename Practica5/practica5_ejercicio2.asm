6012fba2744ea87765fc8a095fc4415a1f9bc259
CarlosOdetteDLCL080301 <carlosodettedlcl@gmail.com>
2023-10-27 18:53:15 -0600

    Se configura correctamente las rotaciones, FALTA programar a las entradas en switch

--- Practica5/practica5_ejercicio2.asm
+++ Practica5/practica5_ejercicio2.asm
@@ -8,9 +8,9 @@ COUNT EQU H'40'
 valor1 equ h'21'
 valor2 equ h'22'
 valor3 equ h'23'
-cte1 equ 40h;Este valor nos ayuda a que en retardo sea de 1/2 segundo (aprox se tarda 0.55 segundos para realizar el cambio)
-cte2 equ 40h
-cte3 equ 40h
+cte1 equ h'01';Este valor nos ayuda a que en retardo sea de 1/2 segundo (aprox se tarda 0.55 segundos para realizar el cambio)
+cte2 equ h'0B'
+cte3 equ h'FF'
 
 retardo 
 	MOVLW cte1
@@ -35,7 +35,48 @@ CASO_CERO:
     MOVWF PORTB     ; Escribe el valor en PORTB
     RETURN
 
-CASO_UNO:;Sentido horario
+PRE_VUELTAS_ANTIHORARIO:;Sentido horario
+;ESTAMOS EN 0 GRADOS
+;VUELTA DE +90 GRADOS, AHORA ROTO 90 GRADOS
+    MOVLW B'10010000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+    CALL retardo
+    MOVLW B'00110000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+    CALL retardo
+    MOVLW B'01100000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+    CALL retardo
+    MOVLW B'11000000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+    CALL retardo
+;VUELTA DE +90 GRADOS, AHORA ROTO 180 GRADOS
+    MOVLW B'10010000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+    CALL retardo
+    MOVLW B'00110000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+    CALL retardo
+    MOVLW B'01100000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+    CALL retardo
+    MOVLW B'11000000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+    CALL retardo
+;VUELTA DE +90 GRADOS, AHORA ROTO 270 GRADOS
+    MOVLW B'10010000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+    CALL retardo
+    MOVLW B'00110000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+    CALL retardo
+    MOVLW B'01100000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+    CALL retardo
+    MOVLW B'11000000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+    CALL retardo
+;VUELTA DE +90 GRADOS, AHORA ROTO 360 GRADOS
     MOVLW B'10010000'
     MOVWF PORTB     ; Escribe el valor en PORTB
     CALL retardo
@@ -50,33 +91,73 @@ CASO_UNO:;Sentido horario
     CALL retardo
     RETURN          ; Regresa de la rutina
 
-CASO_DOS:; Sentido antihorario
-    MOVLW B'10000000'
-    MOVWF PORTB     ; Escribe el valor en PORTB
+PRE_VUELTAS_HORARIO:; Sentido horario
+;ESTAMOS EN 0 GRADOS
+;VUELTA DE +90 GRADOS, AHORA ROTO 90 GRADOS
     MOVLW B'11000000'
     MOVWF PORTB     ; Escribe el valor en PORTB
-    MOVLW B'01000000'
-    MOVWF PORTB     ; Escribe el valor en PORTB
+	CALL retardo
     MOVLW B'01100000'
     MOVWF PORTB     ; Escribe el valor en PORTB
-    MOVLW B'00100000'
-    MOVWF PORTB     ; Escribe el valor en PORTB
+	CALL retardo
     MOVLW B'00110000'
     MOVWF PORTB     ; Escribe el valor en PORTB
-    MOVLW B'00010000'
-    MOVWF PORTB     ; Escribe el valor en PORTB
+	CALL retardo
     MOVLW B'10010000'
     MOVWF PORTB     ; Escribe el valor en PORTB
+	CALL retardo
+;VUELTA DE +90 GRADOS, AHORA ROTO 180 GRADOS
+    MOVLW B'11000000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+	CALL retardo
+    MOVLW B'01100000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+	CALL retardo
+    MOVLW B'00110000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+	CALL retardo
+    MOVLW B'10010000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+	CALL retardo
+;VUELTA DE +90 GRADOS, AHORA ROTO 270 GRADOS
+    MOVLW B'11000000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+	CALL retardo
+    MOVLW B'01100000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+	CALL retardo
+    MOVLW B'00110000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+	CALL retardo
+    MOVLW B'10010000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+	CALL retardo
+;VUELTA DE +90 GRADOS, AHORA ROTO 360 GRADOS
+    MOVLW B'11000000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+	CALL retardo
+    MOVLW B'01100000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+	CALL retardo
+    MOVLW B'00110000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+	CALL retardo
+    MOVLW B'10010000'
+    MOVWF PORTB     ; Escribe el valor en PORTB
+	CALL retardo
     RETURN          ; Regresa de la rutina
 
-CASO_TRES:
+UnaVueltaAntihorario:
+;Se definen las variables
+N equ h'24'
+MOVLW H'180' ; Configura puerto A como entrada
+MOVWF N
 	loop_begin:
 		MOVF N,W
 		BTFSC STATUS,Z
 		GOTO loop_end
 	loop_body:
-		CALL CASO_UNO
-        CALL CASO_UNO
+		CALL PRE_VUELTAS_ANTIHORARIO
 		DECF N,F
 		GOTO loop_begin
 	loop_end:
@@ -87,12 +168,55 @@ CASO_CUATRO:
     MOVWF PORTB     ; Escribe el valor en PORTB
     RETURN          ; Regresa de la rutina
 
-INICIO:
+UnaVueltaHorario:
 ;Se definen las variables
-N equ h'24'
-MOVLW H'5' ; Configura puerto A como entrada
-MOVWF N
+M equ h'25'
+MOVLW H'180' ; Configura puerto A como entrada
+MOVWF M
+	loop_begin_H:
+		MOVF M,W
+		BTFSC STATUS,Z
+		GOTO loop_end_H
+	loop_body_H:
+		CALL PRE_VUELTAS_HORARIO
+		DECF M,F
+		GOTO loop_begin_H
+	loop_end_H:
+    RETURN          ; Regresa de la rutina
 
+vueltasHorario:
+;Se definen las variables
+cantVueltasHorario equ h'26'
+MOVLW H'5' ; Configura puerto A como entrada
+MOVWF cantVueltasHorario
+	loop_begin_cantVueltasHorario:
+		MOVF cantVueltasHorario,W
+		BTFSC STATUS,Z
+		GOTO loop_end_cantVueltasHorario
+	loop_body_cantVueltasHorario:
+		CALL UnaVueltaHorario
+		DECF cantVueltasHorario,F
+		GOTO loop_begin_cantVueltasHorario
+	loop_end_cantVueltasHorario:
+    RETURN          ; Regresa de la rutina
+
+vueltasAntiHorario:
+;Se definen las variables
+cantVueltasAntiHorario equ h'27'
+MOVLW H'A' ; Configura puerto A como entrada
+MOVWF cantVueltasAntiHorario
+	loop_begin_cantVueltasAntiHorario:
+		MOVF cantVueltasAntiHorario,W
+		BTFSC STATUS,Z
+		GOTO loop_end_cantVueltasAntiHorario
+	loop_body_cantVueltasAntiHorario:
+		CALL UnaVueltaAntihorario
+		DECF cantVueltasAntiHorario,F
+		GOTO loop_begin_cantVueltasAntiHorario
+	loop_end_cantVueltasAntiHorario:
+    RETURN          ; Regresa de la rutina
+
+INICIO:
 CLRF PORTA ; Limpia PORTA
 BSF STATUS,RP0 ; Cambia a banco 1
 BCF STATUS,RP1
@@ -103,12 +227,14 @@ MOVWF TRISA
 MOVLW H'0'
 MOVWF TRISB
 BCF STATUS,RP0 ; Cambia al banco 0
-
-CALL CASO_TRES
-
+;Acomodar en la entrada por switch
+;CALL UnaVueltaAntihorario
+;CALL UnaVueltaHorario
+;CALL vueltasHorario
+CALL vueltasAntiHorario
 LECTURA:
 	
-    CALL CASO_TRES
+    ;CALL CASO_TRES
 
 	GOTO LECTURA
 END
